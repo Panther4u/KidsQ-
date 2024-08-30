be5944d2c03cb2df679e42760a92cd6e8246df15
Panther <teampanther4@gmail.com>
2024-08-30 21:19:54 +0530

    save

--- client/src/components/categories/fashion-category.jsx
+++ client/src/components/categories/fashion-category.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Scrollbar, Autoplay } from 'swiper';
 // internal
 import ErrorMsg from "../common/error-msg";
 import { ArrowRightLong } from "@/svg";
 import { HomeTwoCateLoader } from "../loader";
 import { useGetProductTypeCategoryQuery } from "@/redux/features/categoryApi";
+
+// Install Swiper modules
+SwiperCore.use([Scrollbar, Autoplay]);
 
 const FashionCategory = () => {
   const {
@@ -12,7 +17,7 @@ const FashionCategory = () => {
     isLoading,
     isError,
   } = useGetProductTypeCategoryQuery("fashion");
-  const router = useRouter()
+  const router = useRouter();
 
   // handle category route
   const handleCategoryRoute = (title) => {
@@ -24,6 +29,42 @@ const FashionCategory = () => {
         .join("-")}`
     );
   };
+
+  // slider setting
+  const slider_setting = {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true, // Enables continuous loop mode
+    centeredSlides: false,
+    autoplay: {
+      delay: 2000, // Slide delay in milliseconds (3 seconds)
+      disableOnInteraction: false, // Keeps autoplay running even after user interactions
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+      dragClass: "tp-swiper-scrollbar-drag",
+      snapOnRelease: true,
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      576: {
+        slidesPerView: 1,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  };
+
   // decide what to render
   let content = null;
 
@@ -38,41 +79,45 @@ const FashionCategory = () => {
   }
   if (!isLoading && !isError && categories?.result?.length > 0) {
     const category_items = categories.result;
-    content = category_items.map((item) => (
-      <div key={item._id} className="col-xxl-4 col-lg-6">
-        <div className="tp-banner-item-2 p-relative z-index-1 grey-bg-2 mb-20 fix">
-          <div
-            className="tp-banner-thumb-2 include-bg transition-3"
-            style={{ backgroundImage: `url(${item.img})` }}
-          ></div>
-          <h3 className="tp-banner-title-2">
-            <a
-              className="cursor-pointer"
-              onClick={() => handleCategoryRoute(item.parent)}
-            >
-              {item.parent}
-            </a>
-          </h3>
-          <div className="tp-banner-btn-2">
-            <a
-              onClick={() => handleCategoryRoute(item.parent)}
-              className="cursor-pointer tp-btn tp-btn-border tp-btn-border-sm"
-            >
-              Shop Now <ArrowRightLong />
-            </a>
-          </div>
-        </div>
-      </div>
-    ));
+    content = (
+      <Swiper {...slider_setting}>
+        {category_items.map((item) => (
+          <SwiperSlide key={item._id}>
+            <div className="tp-banner-item-2 p-relative z-index-1 grey-bg-2 mb-20 fix">
+              <div
+                className="tp-banner-thumb-2 include-bg transition-3"
+                style={{ backgroundImage: `url(${item.img})` }}
+              ></div>
+              <h3 className="tp-banner-title-2">
+                <a
+                  className="cursor-pointer"
+                  onClick={() => handleCategoryRoute(item.parent)}
+                >
+                  {item.parent}
+                </a>
+              </h3>
+              <div className="tp-banner-btn-2">
+                <a
+                  onClick={() => handleCategoryRoute(item.parent)}
+                  className="cursor-pointer tp-btn tp-btn-border tp-btn-border-sm"
+                >
+                  Shop Now <ArrowRightLong />
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+        <div className="swiper-scrollbar"></div> {/* Scrollbar element */}
+      </Swiper>
+    );
   }
+
   return (
-    <>
-      <section className="tp-banner-area mt-20">
-        <div className="container-fluid tp-gx-40">
-          <div className="row tp-gx-20">{content}</div>
-        </div>
-      </section>
-    </>
+    <section className="tp-banner-area mt-20">
+      <div className="container-fluid tp-gx-40">
+        <div className="row tp-gx-20">{content}</div>
+      </div>
+    </section>
   );
 };
 
