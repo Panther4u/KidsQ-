7f94877384f38d2c5b352083e9ef89c1e85a740b
Nivetha <teampanther4@gmail.com>
2024-09-01 01:00:47 +0530

    map update

--- client/src/components/contact/contact-map.jsx
+++ client/src/components/contact/contact-map.jsx
@@ -16,7 +16,7 @@ const ContactMap = () => {
                   </span>
                 </div>
                 <div className="tp-map-iframe">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830894612!2d-74.11976383964465!3d40.69766374865766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1678114595329!5m2!1sen!2sbd"></iframe>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.092952489349!2d78.0765265!3d10.9563509!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baa2f295004e271%3A0x245178c7dcc6109b!2sLucky%20Furniture!5e0!3m2!1sen!2sin!4v1725111094535!5m2!1sen!2sin"></iframe>
                 </div>
               </div>
             </div>
