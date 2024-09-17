1e1043c5bb7af306d3b87277a59edd707404a0b7
Panther <teampanther4@gmail.com>
2024-09-17 17:40:19 +0530

    save

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.ibb.co','lh3.googleusercontent.com','res.cloudinary.com'],
+    domains: ['i.ibb.co', 'lh3.googleusercontent.com', 'res.cloudinary.com'],
+  },
+  webpack: (config, { isServer }) => {
+    // Add file loader to handle video files like .mp4
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/media/',
+          outputPath: 'static/media/',
+          name: '[name].[hash].[ext]',
+        },
+      },
+    });
+
+    return config;
   },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- client/src/components/banner/fashion-banner.jsx
+++ client/src/components/banner/fashion-banner.jsx
@@ -1,40 +1,127 @@
+// import React from 'react';
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Pagination, EffectFade, Navigation, Autoplay } from 'swiper';
+// import slider_img_1 from '@assets/img/slider/2/slider-1.png';
+// import slider_img_2 from '@assets/img/slider/2/slider-2.png';
+// import slider_img_3 from '@assets/img/slider/2/slider-3.png';
+// import slider_shape from '@assets/img/slider/2/shape/shape-1.png';
+// import thumb_shape_1 from '@assets/img/slider/2/shape/shape-2.png';
+// import thumb_shape_2 from '@assets/img/slider/2/shape/shape-3.png';
+// import styles from './FashionBanner.module.scss';
+
+// const slider_data = [
+//   {
+//     id: 1,
+//     subtitle: 'New Arrivals 2023',
+//     title: 'The Clothing Collection',
+//     img: slider_img_1,
+//   },
+//   {
+//     id: 2,
+//     subtitle: 'Best Selling 2023',
+//     title: 'The Summer Collection',
+//     img: slider_img_2,
+//   },
+//   {
+//     id: 3,
+//     subtitle: 'Winter Has Arrived',
+//     title: 'Amazing New designs',
+//     img: slider_img_3,
+//   },
+// ];
+
+// const slider_setting = {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   effect: 'fade',
+//   navigation: {
+//     nextEl: ".tp-slider-2-button-next",
+//     prevEl: ".tp-slider-2-button-prev",
+//   },
+//   pagination: {
+//     el: ".tp-slider-2-dot",
+//     clickable: true,
+//   },
+//   autoplay: {
+//     delay: 5000,
+//     disableOnInteraction: false,
+//   },
+// };
+
+// const FashionBanner = () => {
+//   return (
+//     <section className={styles.tpSliderArea}>
+//       <Swiper {...slider_setting} modules={[Pagination, Navigation, EffectFade, Autoplay]} className={styles.tpSliderActive2}>
+//         {slider_data.map((item) => (
+//           <SwiperSlide key={item.id}>
+//             <div className={`${styles.tpSliderItem2} ${styles.tpSliderHeight2} `}>
+//               {/* <div className={styles.tpSlider2Shape}>
+//                 <Image className={styles.tpSlider2Shape1} src={slider_shape} alt="slider_shape" />
+//               </div> */}
+//               <div className="row align-items-center mx-1">
+//                 <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+//                   <div className={styles.tpSliderContent2}>
+//                     <span>{item.subtitle}</span>
+//                     <h3 className={styles.tpSliderTitle2}>{item.title}</h3>
+//                     <div className={styles.tpSliderBtn2}>
+//                       <Link href="/shop" className={styles.btn}>Shop Collection</Link>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+//                   <div className={styles.tpSliderThumb2Wrapper}>
+//                     <div className={styles.tpSliderThumb2Shape}>
+//                       <Image className={styles.tpSliderThumb2Shape1} src={thumb_shape_1} alt="shape" />
+//                       <Image className={styles.tpSliderThumb2Shape2} src={thumb_shape_2} alt="shape" />
+//                     </div>
+//                     <div className={`${styles.tpSliderThumb2} text-end`}>
+//                       <span className={styles.tpSliderThumb2Gradient}></span>
+//                       <Image src={item.img} alt="slider img" priority />
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </SwiperSlide>
+//         ))}
+//        <div className="tp-swiper-dot tp-slider-2-dot"></div>
+//       </Swiper>
+//     </section>
+//   );
+// };
+
+// export default FashionBanner;
+
+
+
+
 import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, EffectFade, Navigation, Autoplay } from 'swiper';
-import slider_img_1 from '@assets/img/slider/2/slider-1.png';
-import slider_img_2 from '@assets/img/slider/2/slider-2.png';
-import slider_img_3 from '@assets/img/slider/2/slider-3.png';
-import slider_shape from '@assets/img/slider/2/shape/shape-1.png';
-import thumb_shape_1 from '@assets/img/slider/2/shape/shape-2.png';
-import thumb_shape_2 from '@assets/img/slider/2/shape/shape-3.png';
-import styles from './FashionBanner.module.scss';
+import slider_video_1 from '@assets/videos/slider-video-3.mp4'; 
+import slider_video_2 from '@assets/videos/slider-video-2.mp4';
+import slider_video_3 from '@assets/videos/slider-video-3.mp4';
 
 const slider_data = [
   {
     id: 1,
-    subtitle: 'New Arrivals 2023',
-    title: 'The Clothing Collection',
-    img: slider_img_1,
+    video: slider_video_1,
   },
   {
     id: 2,
-    subtitle: 'Best Selling 2023',
-    title: 'The Summer Collection',
-    img: slider_img_2,
+    video: slider_video_2,
   },
   {
     id: 3,
-    subtitle: 'Winter Has Arrived',
-    title: 'Amazing New designs',
-    img: slider_img_3,
+    video: slider_video_3,
   },
 ];
 
 const slider_setting = {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 0,
   effect: 'fade',
   navigation: {
     nextEl: ".tp-slider-2-button-next",
@@ -52,41 +139,32 @@ const slider_setting = {
 
 const FashionBanner = () => {
   return (
-    <section className={styles.tpSliderArea}>
-      <Swiper {...slider_setting} modules={[Pagination, Navigation, EffectFade, Autoplay]} className={styles.tpSliderActive2}>
+    <section style={{ position: 'relative', width: '100%', height: '300px', overflow: 'hidden' }} className='my-3'>
+      <Swiper {...slider_setting} modules={[Pagination, Navigation, EffectFade, Autoplay]} className="tpSliderActive2">
         {slider_data.map((item) => (
           <SwiperSlide key={item.id}>
-            <div className={`${styles.tpSliderItem2} ${styles.tpSliderHeight2} `}>
-              {/* <div className={styles.tpSlider2Shape}>
-                <Image className={styles.tpSlider2Shape1} src={slider_shape} alt="slider_shape" />
-              </div> */}
-              <div className="row align-items-center mx-1">
-                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
-                  <div className={styles.tpSliderContent2}>
-                    <span>{item.subtitle}</span>
-                    <h3 className={styles.tpSliderTitle2}>{item.title}</h3>
-                    <div className={styles.tpSliderBtn2}>
-                      <Link href="/shop" className={styles.btn}>Shop Collection</Link>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
-                  <div className={styles.tpSliderThumb2Wrapper}>
-                    <div className={styles.tpSliderThumb2Shape}>
-                      <Image className={styles.tpSliderThumb2Shape1} src={thumb_shape_1} alt="shape" />
-                      <Image className={styles.tpSliderThumb2Shape2} src={thumb_shape_2} alt="shape" />
-                    </div>
-                    <div className={`${styles.tpSliderThumb2} text-end`}>
-                      <span className={styles.tpSliderThumb2Gradient}></span>
-                      <Image src={item.img} alt="slider img" priority />
-                    </div>
-                  </div>
-                </div>
+            <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                  <div style={{ position: 'relative', width: '100%', height: '100%', textAlign: 'end' }}>
+                    <video
+                      src={item.video}
+                      autoPlay
+                      loop
+                      muted
+                      playsInline
+                      style={{
+                        objectFit: 'cover',
+                        width: '100%',
+                        height: '300px',
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                      }}
+                    />
               </div>
             </div>
           </SwiperSlide>
         ))}
-       <div className="tp-swiper-dot tp-slider-2-dot"></div>
+        <div className="tp-swiper-dot tp-slider-2-dot"></div>
       </Swiper>
     </section>
   );
